20122a1aac6cef7de92cd64f0e7d2999f8244a52
Sümer Cip <sumerc@gmail.com>
2011-03-07 11:28:52 +0200

    PYPI upload. download format changed --> tar.gz

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from distutils.core import setup, Extension
 
-setup(name="_yappi", 
-    version="0.5",
-    description="Yet Another Python Profiler",
+f = open('README')
+long_description = f.read()
+
+HOMEPAGE = "http://yappi.googlecode.com/"
+NAME = "yappi"
+VERSION = "0.53"
+
+setup(name=NAME, 
+    version=VERSION,    
     author="Sumer Cip",
     author_email="sumerc@gmail.com",
     ext_modules = [Extension
@@ -10,6 +16,7 @@ setup(name="_yappi",
             sources = ["_yappi.c", "_ycallstack.c", 
                 "_yhashtab.c", "_ymem.c", "_yfreelist.c", 
                 "_ytiming.c"],
+            depends = ["_ycallstack.h"],
             #define_macros=[('DEBUG_MEM', '1'), ('DEBUG_CALL', '1'), ('YDEBUG', '1')],
             #define_macros=[('YDEBUG', '1')],
             #define_macros=[('DEBUG_CALL', '1')],
@@ -18,5 +25,11 @@ setup(name="_yappi",
             #extra_compile_args = ["TEST"]
         )
     ],
-    py_modules =  ["yappi"]
+    py_modules =  ["yappi"],
+    description="Yet Another Python Profiler",
+    long_description = long_description,
+    keywords = "python multithread profile",
+    license = "MIT",
+    url = HOMEPAGE,
+    download_url = "%s/files/%s-%s.tar.gz" % (HOMEPAGE, NAME, VERSION),
 )
