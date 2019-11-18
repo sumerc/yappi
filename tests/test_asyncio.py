7d94a3ac79543cac15276d69480b85a1f860f9c2
David Montague <david@exactrics.com>
2019-11-17 23:50:37 -0800

    Add various tests

--- tests/test_asyncio.py
+++ tests/test_asyncio.py
@@ -1,7 +1,8 @@
 import asyncio
+import contextvars
+import functools
 import time
 import unittest
-from contextvars import ContextVar
 
 import utils
 import yappi
@@ -63,7 +64,7 @@ class MultipleEventLoopTest(YappiUnitTestCase):
         pass
 
 
-async_context_id = ContextVar('async_context_id')
+async_context_id = contextvars.ContextVar('async_context_id')
 async_context_id.set(-1)
 
 
@@ -76,41 +77,111 @@ def get_async_context_id():
 
 class AsyncUsage(utils.YappiUnitTestCase):
     task_count = 5
+    context_switch_count = 2
     duration = 0.1
+    task_to_profile_name = "task_to_profile"
 
     def test_async_cpu(self):
-        self._run_and_validate("cpu")
+        ttots = self.profile_tasks("cpu")
+        for ttot in ttots:
+            self.assertLessEqual(ttot, self.duration * 0.1)
 
     def test_async_wall(self):
-        self._run_and_validate("wall")
+        ttots = self.profile_tasks("wall")
+        for ttot in ttots:
+            self.assertGreaterEqual(ttot, self.duration)
 
-    def _run_and_validate(self, clock_type):
+    def profile_tasks(self, clock_type):
         yappi.set_clock_type(clock_type)
-        yappi.set_context_id_callback(get_async_context_id)
-        asyncio.run(self._run_async_calls())
-        for i in range(self.task_count):
-            fstats = yappi.get_func_stats(filter={"ctx_id": i, "name": "sleep_for"})
-            stats = fstats.pop()
-            self.assertEqual(stats.ncall, 2)  # Change to 1 if context switches should not count as calls
-            if clock_type == "wall":
-                self.assertGreaterEqual(stats.ttot, self.duration)
-            else:
-                assert clock_type == "cpu"
-                self.assertLessEqual(stats.ttot, self.duration * 0.01)
+        asyncio.run(self.profile_tasks_coroutine())
+        return self.get_profiling_outputs()
 
-    async def _run_async_calls(self):
+    async def profile_tasks_coroutine(self):
+        yappi.start()
+        tasks = self.get_tasks()
+        await asyncio.gather(*tasks)
+        yappi.stop()
+
+    def get_profiling_outputs(self):
+        fstats = yappi.get_func_stats(filter={"name": self.task_to_profile_name})
+        stats = fstats.pop()
+        self.assertEqual(stats.ncall, self.task_count * self.context_switch_count)
+
+        averaged_times = [stats.ttot / self.task_count for _ in range(self.task_count)]
+        return averaged_times
+
+    def get_tasks(self):
+        return [self.task_to_profile(self.duration) for _ in range(self.task_count)]
+
+    @staticmethod
+    async def task_to_profile(duration: float):
+        await asyncio.sleep(duration)
+
+
+class ContextVarsMixin:
+    task_count: int
+    duration: float
+    task_to_profile_name: str
+    context_switch_count: int
+
+    async def profile_tasks_coroutine(self):
+        yappi.set_context_id_callback(get_async_context_id)
+        await super().profile_tasks_coroutine()
+
+    def get_profiling_outputs(self):
+        ttots = []
+        for i in range(self.task_count):
+            fstats = yappi.get_func_stats(filter={"ctx_id": i, "name": self.task_to_profile_name})
+            stats = fstats.pop()
+            self.assertEqual(stats.ncall, self.context_switch_count)
+            ttots.append(stats.ttot)
+        return ttots
+
+    def get_tasks(self):
         async def await_with_context_id(context_id, awaitable):
             async_context_id.set(context_id)
             return await awaitable
 
-        async def sleep_for(time: float):
-            await asyncio.sleep(time)
+        return [await_with_context_id(i, self.task_to_profile(self.duration)) for i in range(self.task_count)]
 
-        yappi.start()
-        tasks = [await_with_context_id(i, sleep_for(self.duration)) for i in range(self.task_count)]
-        await asyncio.gather(*tasks)
-        yappi.stop()
 
+class ThreadTaskMixin:
+    run_in_threadpool_copy_contextvars = False
+    task_to_profile_name = "_task_in_thread"
+    context_switch_count = 1
+
+    async def task_to_profile(self, duration: float):
+        await run_in_threadpool(self.run_in_threadpool_copy_contextvars, self._task_in_thread, duration)
+
+    @staticmethod
+    def _task_in_thread(duration: float):
+        time.sleep(duration)
+
+
+class AsyncUsageWithContextvars(ContextVarsMixin, AsyncUsage):
+    pass
+
+
+class AsyncUsageThreaded(ThreadTaskMixin, AsyncUsage):
+    pass
+
+
+class AsyncUsageThreadedWithContextvars(ThreadTaskMixin, ContextVarsMixin, AsyncUsage):
+    run_in_threadpool_copy_contextvars = True
+
+
+async def run_in_threadpool(copy_contextvars, func, *args, **kwargs):
+    """Based on the starlette function of the same name"""
+    loop = asyncio.get_event_loop()
+    if copy_contextvars:
+        # Ensure we run in the same context
+        child = functools.partial(func, *args, **kwargs)
+        context = contextvars.copy_context()
+        func = context.run
+        args = (child,)
+    elif kwargs:
+        func = functools.partial(func, **kwargs)
+    return await loop.run_in_executor(None, func, *args)
 
 if __name__ == '__main__':
     unittest.main()
