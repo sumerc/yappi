dd867cae4ac5d16a9da222ad7c1df2ca26aaaad4
Sümer Cip <sumer.cip@platform.sh>
2023-12-07 11:32:53 +0300

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.md', encoding='UTF-8') as f:
 
 HOMEPAGE = "https://github.com/sumerc/yappi"
 NAME = "yappi"
-VERSION = "1.5.1"
+VERSION = "1.6.0"
 _DEBUG = False  # compile/link code for debugging
 _PROFILE = False  # profile yappi itself
 
